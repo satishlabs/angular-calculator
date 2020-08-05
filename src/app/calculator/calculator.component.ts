1babf887f691ce83b43708835abb60e6194a521a
Satish Prasad <spd61185@gmail.com>
2020-08-05 15:44:33 +0530

    angular-calculator

---
+++ src/app/calculator/calculator.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NgModule }      from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent {
+
+  public num1:number;
+  public num2:number;
+  public result:number;
+
+  add(){
+    this.result = this.num1 + this.num2;
+  }
+  subtract(){
+    this.result = this.num1 - this.num2;
+  }
+  multiply(){
+    this.result = this.num1 * this.num2;
+  }
+  divide(){
+    this.result = this.num1 / this.num2;
+  }
+}
